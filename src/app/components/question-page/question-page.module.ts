8c2e3d164c3d1eca49aaf82cbf3f493c3fcade86
Júlia Peixer <132520390+JuliaPeixerr@users.noreply.github.com>
2024-06-28 22:47:15 +0000

    ⭐Novo layout da tela

--- src/app/components/question-page/question-page.module.ts
+++ src/app/components/question-page/question-page.module.ts
@@ -1,10 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { QuestionPageComponent } from './question-page.component';
-import { AnswerDialog } from './dialog/answer.component';
 
 @NgModule({
-    declarations: [QuestionPageComponent, AnswerDialog],
+    declarations: [QuestionPageComponent],
     exports: [QuestionPageComponent],
     imports: [
         CommonModule
